6f63e1b3ce7fdfeed8280aa61f888c88c4714018
art-em-qa <a.embulaev@quality-lab.ru>
2022-03-12 12:40:26 +0300

    Объекту Point добавлен метод distance

--- sandbox/src/main/java/DistancePoint2D.java
+++ sandbox/src/main/java/DistancePoint2D.java
@@ -2,9 +2,6 @@ public class DistancePoint2D {
     public static void main(String[] args) {
         Point p1 = new Point (1, 2);
         Point p2 = new Point (2,4);
-        System.out.println((distance(p1,p2)));
-    }
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+        System.out.println(p1.distance(p2));
     }
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,8 +2,11 @@ public class Point{
     public double x;
     public double y;
 
-    public Point(double x, double y){
-        this.x = x;
-        this.y = y;
+    public Point (double x1, double y1){
+        this.x = x1;
+        this.y = y1;;
+    }
+    public double distance(Point p2){
+        return Math.sqrt((p2.x - this.x)*(p2.x - this.x) + (p2.y - this.y)*(p2.y - this.y));
     }
 }
