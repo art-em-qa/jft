0fbd95aeb3ae9e7bf7921c01789fde92e9ae4a55
art-em-qa <a.embulaev@quality-lab.ru>
2022-03-14 22:38:04 +0300

    Создан тест и вспомогательный класс UserData для создания нового юзера

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+    private final String firstname;
+    private final String lastname;
+    private final String address;
+    private final String workphone;
+    private final String email;
+
+    public UserData(String firstname, String lastname, String address, String workphone, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.workphone = workphone;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getWorkphone() {
+        return workphone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
