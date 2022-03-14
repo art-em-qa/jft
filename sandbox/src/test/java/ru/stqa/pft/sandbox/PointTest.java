6fe4a7eb5328d9c827869dccb98ff9a0aed5eb03
art-em-qa <a.embulaev@quality-lab.ru>
2022-03-14 10:16:29 +0300

    Добавлен еще 1 тест для класса Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -11,4 +11,10 @@ public class PointTest {
         Point p2 = new Point(2,4);
         Assert.assertEquals(p1.distance(p2), 2.23606797749979);
     }
+    @Test
+    public void testDistance2(){
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(8,10);
+        Assert.assertEquals(p1.distance(p2), 8.48528137423857);
+    }
 }
