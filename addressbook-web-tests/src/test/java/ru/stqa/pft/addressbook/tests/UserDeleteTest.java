87e912703939d12ff1aa83476a1ef717908b66a4
art-em-qa <a.embulaev@quality-lab.ru>
2022-03-31 20:59:00 +0300

    Доработан тест удаления контакта. Если нет контакта и группы теперь создаем группу, затем пользователя, и удаляем пользователя

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeleteTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserDeleteTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserData;
 
 
@@ -9,6 +10,12 @@ public class UserDeleteTest extends TestBase{
   @Test
   public void testUserDelete() throws Exception {
     if(! app.getContactHelper().isThereAContact()){
+      app.getContactHelper().initCreateNewUser();
+      if(! app.getContactHelper().isGroupCreated(1)){
+        app.getNavigationHelper().openGroupPage();
+        app.getGroupHelper().createGroup(new GroupData("peopleTest", "humanTest", "testGroup"));
+        app.getNavigationHelper().returnHomePage();
+      }
       app.getContactHelper().createContact(new UserData("Antonio", "Fagundes",
               "Portugal, St.Barbara", "+0123456789", "a.fagundes@stbarbara.com",
               "peopleTest"));
