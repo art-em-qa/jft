55b0064c97b837c08fd8300a2eafd1f0ced82646
art-em-qa <a.embulaev@quality-lab.ru>
2022-04-17 19:39:37 +0300

    Коммит для проверки почему не работает получение данных из xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -21,6 +21,9 @@ public class GroupDataGenerator {
     @Parameter(names = "-f", description = "Target file")
     public String file;
 
+    @Parameter(names = "-d", description = "Data format")
+    public String format;
+
     public static void main(String[] args) throws IOException {
         GroupDataGenerator generator = new GroupDataGenerator();
         JCommander jCommander = new JCommander(generator);
@@ -35,7 +38,23 @@ public class GroupDataGenerator {
 
     private void run() throws IOException {
         List<GroupData> groups = generateGroups(count);
-        save(groups, new File(file));
+        if(format.equals("csv")){
+            saveAsCsv(groups, new File(file));
+        } else if (format.equals("xml")){
+            saveAsXml(groups, new File(file));
+        }else{
+            System.out.println("Неизвестный формат" + format);
+        }
+
+    }
+
+    private void saveAsXml(List<GroupData> groups, File file) throws IOException {
+        XStream xStream =new XStream();
+        xStream.processAnnotations(GroupData.class);
+        String xml = xStream.toXML(groups);
+        Writer writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
     }
 
     private static List<GroupData> generateGroups(int count) {
@@ -47,7 +66,7 @@ public class GroupDataGenerator {
         return groups;
     }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private static void saveAsCsv(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for (GroupData group : groups) {
             writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
