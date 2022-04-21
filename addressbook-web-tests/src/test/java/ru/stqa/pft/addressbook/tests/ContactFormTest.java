a97c1cd24d13efea091a2460cc120aec89881667
art-em-qa <a.embulaev@quality-lab.ru>
2022-04-21 21:00:00 +0300

    реализовано получение данных о группах из БД

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactFormTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactFormTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class ContactFormTest extends TestBase {
-    
+
     @BeforeMethod
     public void ensurePreconditions() {
         if (app.contact().all().size() == 0) {
